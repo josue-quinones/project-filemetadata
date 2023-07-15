9a4103fae7236ad179ce8da333115019cf851683
Josue Quinones <jquinon1@emich.edu>
2023-07-15 14:09:48 -0400

    Project Upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
 
 var app = express();
@@ -12,6 +14,13 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  res.json({
+    "name":req.file.originalname,
+    "type":req.file.mimetype,
+    "size":req.file.size
+  })
+});
 
 
 const port = process.env.PORT || 3000;
